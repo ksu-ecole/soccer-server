ebb490f5c5d7347ae137151ef59781469e8716f8
alls7554 <alls970459@gmail.com>
2020-12-10 00:39:43 +0900

    delete cascade

--- src/main/java/com/ksu/soccerserver/team/Team.java
+++ src/main/java/com/ksu/soccerserver/team/Team.java
@@ -45,7 +45,7 @@ public class Team {
     private Account owner;
 
     // 팀에 소속된 인원들과의 관계성
-    @OneToMany(mappedBy = "team", cascade = CascadeType.PERSIST)
+    @OneToMany(mappedBy = "team")
     private final Set<Account> accounts = new HashSet<>();
 
     // 팀에 가입신청한 유저들의 리스트를 나타내는 관계성
