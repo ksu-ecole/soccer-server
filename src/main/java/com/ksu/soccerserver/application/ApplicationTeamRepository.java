aff488f4cec3096b099d49dcb81f8749dda13dec
alls7554 <alls970459@gmail.com>
2020-12-11 15:12:19 +0900

    modify config, create optional<ApplicationTeam>

--- src/main/java/com/ksu/soccerserver/application/ApplicationTeamRepository.java
+++ src/main/java/com/ksu/soccerserver/application/ApplicationTeamRepository.java
@@ -2,9 +2,11 @@ package com.ksu.soccerserver.application;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface ApplicationTeamRepository extends JpaRepository<ApplicationTeam, Long> {
     List<ApplicationTeam> findByApplyTeamsId(Long applyTeamsId);
     List<ApplicationTeam> findAllByMatchId(Long matchId);
-    ApplicationTeam findByMatchId(Long matchId);
+   ApplicationTeam  findByMatchId(Long matchId);
+   Optional<ApplicationTeam> findByMatchIdAndApplyTeamsId(Long matchId, Long applyTeamsId);
 }
--- src/main/java/com/ksu/soccerserver/config/SecurityConfig.java
+++ src/main/java/com/ksu/soccerserver/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.PUT, "/api/applications/accounts/**/team", "/api/matches/**", "/api/teams/**").hasAnyRole("LEADER")
                 .antMatchers(HttpMethod.DELETE, "/api/matches/**", "/api/teams/**").hasAnyRole("LEADER")
                 .antMatchers(HttpMethod.GET,"/api/applications/teams/**").hasAnyRole("LEADER")
-                .antMatchers(HttpMethod.PUT,"/api/applications/teams/**").hasAnyRole("LEADER")
+                .antMatchers(HttpMethod.PUT,"/api/applications/teams/**/away").hasAnyRole("LEADER")
 
                 // USER or LEADER
                 .anyRequest().authenticated()
